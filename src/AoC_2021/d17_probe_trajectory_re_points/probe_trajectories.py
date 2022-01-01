16ce3a71c1ddb240c5aedeb409ffb533659cbd63
derailed-dash <derailed.dash@gmail.com>
2022-01-01 22:33:49 +0000

    Refactoring of day 17

--- src/AoC_2021/d17_probe_trajectory_re_points/probe_trajectories.py
+++ src/AoC_2021/d17_probe_trajectory_re_points/probe_trajectories.py
@@ -4,17 +4,32 @@ Date: 17/12/2021
 
 Solving https://adventofcode.com/2021/day/17
 
-Firing a probe with velocity x,y.  +ve y means up.
-x velocity decreases by 1 towards 0 with each step.
-y velocity always decreases by 1 (i.e. if -ve, will get more -ve)
+We need to fire a probe into a large ocean trench. 
+Firing from 0,0 with velocity x,y.  +ve y means up.
+x and y are distance travelled by per unit t.
+x velocity decreases by 1 towards 0 with each step, due to drag.
+y velocity always decreases by 1, due to gravity. So if -ve, will get more -ve.
 
-Here we've gone with a brute force solution.  Takes a few seconds.
-Suspect some optimisation could be done.
+To enter the trench, the probe must be on a trajectory that causes it to 
+pass through a target area. It must be WITHIN the target area at any given value of t.
+
+Target area looks like: x=20..30, y=-10..-5       20,-5 xxxxxxxxxxx 30,-5
+                                                        xxxxxxxxxxx
+                                                        xxxxxxxxxxx
+                                                        xxxxxxxxxxx
+                                                        xxxxxxxxxxx
+                                                 20,-10 xxxxxxxxxxx 30,-10
+
+I.e. a rectangular range that is inclusive of the x and y values provided.
 
 Part 1:
-    First, use a formula to calculate velocity at step s.
-    Then, loop through range of initial velocitise, with x from 0 to max x+1.
-    and y from lowest target y to abs(lowest target y).
+    What is the highest y position we can achieve on any successful trajectory?
+
+    First, use a formula to calculate velocity at step t, given an initial velocity.
+    Then, loop through range of initial velocities, with:
+    - x from 0 to x_right, since we can only fire right, towards the target.
+    - y from bottom_y to abs(bottom_y), since we can fire both down towards the target, 
+      but also up.
     Then, loop infinitely through steps, and determine x,y locations at each step.
     Store max y achieved in this loop so far.
     Build a dict of {init vel: max y} for any init velocities that hit the target.
@@ -22,16 +37,32 @@ Part 1:
     Finally, return the max y overall.
 
 Part 2:
-    Already solved in part 1.
+    Find every initial velocity that causes the probe to be within the target area
+    at any time t.
 """
 import logging
 import os
 import time
 import re
-from collections import namedtuple
+from typing import NamedTuple
 
-Point = namedtuple("Pnt", "x y")  # Make it easier to index point x and y
-Velocity = namedtuple("Vel", "x y")
+class Point(NamedTuple):
+    x: int
+    y: int
+    
+class Velocity(Point):
+    """ A vector represented as (x, y) values """
+    
+class Rect(NamedTuple):
+    """ Rectangle from four corner Points, and knows whether a point is enclosed """
+    left_x: int
+    right_x: int
+    bottom_y: int
+    top_y: int
+
+    def encloses(self, point:Point) -> bool:
+        return (self.left_x <= point.x <= self.right_x 
+                and self.bottom_y <= point.y <= self.top_y)
 
 SCRIPT_DIR = os.path.dirname(__file__) 
 INPUT_FILE = "input/input.txt"
@@ -47,48 +78,52 @@ def main():
     with open(input_file, mode="rt") as f:
         data = f.read().strip()
     
+    # Note that x and y values can be -ve
     match = re.search(r"x=(-?\d+)\.\.(-?\d+), y=(-?\d+)\.\.(-?\d+)", data)
-    # Note: x_min < x_max and y_min < y_max, but but abs(y_min) > abs(y_max)
-    # I.e. y_min is the BOTTOM of the target
-    t_x_min, t_x_max, t_y_min, t_y_max = map(int, match.groups())
-    logger.info("x_min=%d, x_max=%d, y_min=%d, y_max=%d", t_x_min, t_x_max, t_y_min, t_y_max)
-    
-    start = Point(0,0)  # Where we launch our probe from
+    target = Rect(*map(int, match.groups()))
+    logger.info(target)
     
     successful_peaks = {}    # init_velocity: peak
-    for x in range(1, t_x_max+1):   # No point having x larger than max target distance
-        for y in range(t_y_min, abs(t_y_min)):   # remember we can fire up
-            initial_v = Velocity(x,y)
-            step = 0
-            location = start
-            max_y_so_far_this_route = t_y_min
-            while True:
-                vel = velocity_at_step(initial_v, step)
-                location = Point(location.x + vel.x, location.y + vel.y)             
-                max_y_so_far_this_route = max(location.y, max_y_so_far_this_route)
-                
-                if (vel.x == 0 and location.x < t_x_min):
-                    break   # we're just going to fall downwards from here.
-                
-                # If we've missed the target
-                if location.x > t_x_max or location.y < t_y_min:
-                    break   # we've missed the target
-                
-                # If we've hit the target
-                if t_x_min <= location.x <= t_x_max and t_y_min <= location.y <= t_y_max:
-                    successful_peaks[initial_v] = max_y_so_far_this_route # store our successful peak
-                    # logger.debug("Initial vel=%s, peak=%d", initial_v, max_y_so_far_this_route)
-                    break
-                
-                step += 1
+    for x in range(1, target.right_x+1):   # No point having x larger than max target distance
+        for y in range(target.bottom_y, abs(target.bottom_y)):   # remember we can fire up
+            init_v = Velocity(x, y)
+            hit, res = evaluate_trajectory(target, init_v)
+            if hit:
+                successful_peaks[init_v] = res
     
     logger.info("Max peak=%d", max(successful_peaks.values()))
     logger.info("Count of valid shots=%d", sum(1 for peak in successful_peaks))
+
+def evaluate_trajectory(target: Rect, initial_v: Velocity) -> tuple[bool, int]:
+    t = 0
+    location = Point(0,0)  # Where we launch our probe from
+    max_y_so_far_this_trajectory = target.bottom_y
+    hit_target = False
+    
+    while not hit_target:
+        vel = velocity_at_step(initial_v, t)
+        location = Point(location.x + vel.x, location.y + vel.y)             
+        max_y_so_far_this_trajectory = max(location.y, max_y_so_far_this_trajectory)
                 
-def velocity_at_step(init: Velocity, n: int) -> Velocity:
+        if (vel.x == 0 and location.x < target.left_x):
+            break   # we're just going to fall downwards from here.
+                
+        if location.x > target.right_x or location.y < target.bottom_y:
+            break   # we've overshot the target
+                
+        if target.encloses(location): # If we've hit the target
+            hit_target = True
+            break
+
+        # If we're here, we haven't yet reached the target
+        t += 1
+        
+    return hit_target, max_y_so_far_this_trajectory
+                
+def velocity_at_step(init: Velocity, t: int) -> Velocity:
     """ Returns the velocity (x,y) at a given step. """
-    x = abs(init.x) - n if n < init.x else 0
-    y = init.y - n
+    x = abs(init.x) - t if t < init.x else 0
+    y = init.y - t
     
     return Velocity(x, y)
 
