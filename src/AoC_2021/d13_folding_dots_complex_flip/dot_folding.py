6b3f0c291ef0301d77dd87c5354c63b34608d8a8
derailed-dash <derailed.dash@gmail.com>
2022-01-20 18:45:41 +0000

    Converted day 13 to class and added vis

--- src/AoC_2021/d13_folding_dots_complex_flip/dot_folding.py
+++ src/AoC_2021/d13_folding_dots_complex_flip/dot_folding.py
@@ -33,74 +33,101 @@ Part 2:
 import logging
 import os
 import time
+from matplotlib import pyplot as plt
 
 SCRIPT_DIR = os.path.dirname(__file__) 
 INPUT_FILE = "input/input.txt"
 # INPUT_FILE = "input/sample_input.txt"
 
-logging.basicConfig(level=logging.DEBUG, 
-                    format="%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:\t%(message)s", 
+logging.basicConfig(format="%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:\t%(message)s", 
                     datefmt='%Y-%m-%d %H:%M:%S')
 logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
+
+class Paper():
+    """ Represents transparent paper with dots at specified x,y locations.
+    The paper knows how to fold itself, given an instruction with an x or y value to fold along. """
+    def __init__(self, dots: set[complex]) -> None:
+        self._dots: set[complex] = dots
+    
+    @property
+    def dot_count(self) -> int:
+        """ Total number of dots showing on the paper """
+        return len(self._dots)
+    
+    def __str__(self) -> str:
+        """ Convert the dots to a printable string """
+        height = int(max(self._dots, key=lambda y: y.imag).imag)
+        width = int(max(self._dots, key=lambda x: x.real).real)
+        
+        rows = []
+        for row in range(height+1):
+            row_str = ""
+            for col in range(width+1):
+                coord = complex(col, row)
+                row_str += "#" if coord in self._dots else " "
+
+            rows.append(row_str)
+        
+        return "\n".join(rows)
+
+    def render_as_plt(self):
+        """ Render this paper and its dots as a scatter plot """
+        all_x = [point.real for point in self._dots]
+        all_y = [point.imag for point in self._dots]
+        
+        axes = plt.gca()
+        axes.set_aspect('equal')
+        plt.axis("off") # hide the border around the plot axes
+        axes.set_xlim(min(all_x)-1, max(all_x)+1)
+        axes.set_ylim(min(all_y)-1, max(all_y)+1)
+        axes.invert_yaxis()
+        
+        axes.scatter(all_x, all_y, marker="o", s=50)
+        plt.show()
+    
+    def fold(self, instruction: list[str]):
+        """ Fold along a given axis.  Returns the union set of 
+        numbers before the fold line, and the flip of the numbers after the fold line.
+
+        Args:
+            instruction (list[str]): x (vertical) or y (horizontal), value of fold axis
+        """
+        instr, axis = instruction[0], int(instruction[1])
+        assert instr in ('x', 'y'), "Instruction must be 'x' or 'y'"
+        
+        before_fold = set()
+        after_fold = set()
+        
+        if instr == 'x':    # fold vertical
+            before_fold = set(dot for dot in self._dots if dot.real < axis)
+            after_fold = set(num for num in self._dots if num.real > axis)
+            folded = set(complex(axis-(num.real-axis), num.imag) for num in after_fold)
+        else:   # fold horizontal
+            before_fold = set(num for num in self._dots if num.imag < axis)
+            after_fold = set(num for num in self._dots if num.imag > axis)
+            folded = set(complex(num.real, axis-(num.imag-axis)) for num in after_fold)
+        
+        self._dots = before_fold | folded   
 
 def main():
     input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
     with open(input_file, mode="rt") as f:
-        data = f.read()
-    
-    dots, instructions = process_data(data)
+        dots, instructions = process_data(f.read())
 
+    paper = Paper(dots)
+    
     # Part 1 - First instruction only
-    dot_set = fold(dots, instructions[0])
-    logger.info("Part 1: %d dots are visible", len(dot_set))
+    paper.fold(instructions[0])
+    logger.info("Part 1: %d dots are visible", paper.dot_count)
 
     # Part 2 - All remaining instructions
     for instruction in instructions[1:]:
-        dot_set = fold(dot_set, instruction)
+        paper.fold(instruction)
         
-    logger.info("Part 2: %d dots are visible", len(dot_set))
-    logger.info("Part 1 decoded:\n%s", render_as_str_array(dot_set))
-    
-def render_as_str_array(dots: set[complex]) -> str:
-    """ Convert the dots to a printable grid """
-    height = int(max(dots, key=lambda y: y.imag).imag)
-    width = int(max(dots, key=lambda x: x.real).real)
-    
-    rows = []
-    for row in range(height+1):
-        row_str = ""
-        for col in range(width+1):
-            coord = complex(col, row)
-            row_str += "#" if coord in dots else " "
-
-        rows.append(row_str)
-    
-    return "\n".join(rows) 
-    
-def fold(nums: set[complex], instruction: list[str]) -> set[complex]:
-    """ Fold along a given axis.  Returns the union set of 
-    numbers before the fold line, and the flip of the numbers after the fold line.
-
-    Args:
-        nums (set[complex]): Numbers to fold
-        instruction (list[str]): x (vertical) or y (horizontal), value of fold axis
-    """
-    instr, axis = instruction[0], int(instruction[1])
-    assert instr in ('x', 'y'), "Instruction must be 'x' or 'y'"
-    
-    before_fold = set()
-    after_fold = set()
-    
-    if instr == 'x':    # fold vertical
-        before_fold = set(num for num in nums if num.real < axis)
-        after_fold = set(num for num in nums if num.real > axis)
-        folded = set(complex(axis-(num.real-axis), num.imag) for num in after_fold)
-    else:   # fold horizontal
-        before_fold = set(num for num in nums if num.imag < axis)
-        after_fold = set(num for num in nums if num.imag > axis)
-        folded = set(complex(num.real, axis-(num.imag-axis)) for num in after_fold)
-    
-    return before_fold | folded    
+    logger.info("Part 2: %d dots are visible", paper.dot_count)
+    logger.info("Part 2 decoded:\n%s", paper)
+    paper.render_as_plt()
 
 def process_data(data: str) -> tuple[set[complex], list]:
     """ Input has n rows of x,y coords, then an empty line, then rows of instructions """
