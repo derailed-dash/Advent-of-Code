eb7002a01aefa4cbb55a116f8592d57e0b46ee5d
derailed-dash <derailed.dash@gmail.com>
2022-01-20 21:40:24 +0000

    Improving day 13 and doc done

--- src/AoC_2021/d13_folding_dots_complex_flip/dot_folding.py
+++ src/AoC_2021/d13_folding_dots_complex_flip/dot_folding.py
@@ -30,6 +30,7 @@ Part 2:
     Work out size of the printable area from max real and imag components.
     Then print a grid of this size, but printing # if the coord exists in the complex set.
 """
+from dataclasses import dataclass
 import logging
 import os
 import time
@@ -44,11 +45,22 @@ logging.basicConfig(format="%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:\t%(m
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.DEBUG)
 
+@dataclass
+class Instruction:
+    """ Paper fold instruction """
+    axis: str   # x or y
+    val: int
+    
+@dataclass(frozen=True)
+class Point:
+    x: int
+    y: int
+
 class Paper():
     """ Represents transparent paper with dots at specified x,y locations.
     The paper knows how to fold itself, given an instruction with an x or y value to fold along. """
-    def __init__(self, dots: set[complex]) -> None:
-        self._dots: set[complex] = dots
+    def __init__(self, dots: set[Point]) -> None:
+        self._dots: set[Point] = dots
     
     @property
     def dot_count(self) -> int:
@@ -57,14 +69,14 @@ class Paper():
     
     def __str__(self) -> str:
         """ Convert the dots to a printable string """
-        height = int(max(self._dots, key=lambda y: y.imag).imag)
-        width = int(max(self._dots, key=lambda x: x.real).real)
+        height = max(point.y for point in self._dots)
+        width = max(point.x for point in self._dots)
         
         rows = []
         for row in range(height+1):
             row_str = ""
             for col in range(width+1):
-                coord = complex(col, row)
+                coord = Point(col, row)
                 row_str += "#" if coord in self._dots else " "
 
             rows.append(row_str)
@@ -73,8 +85,8 @@ class Paper():
 
     def render_as_plt(self):
         """ Render this paper and its dots as a scatter plot """
-        all_x = [point.real for point in self._dots]
-        all_y = [point.imag for point in self._dots]
+        all_x = [point.x for point in self._dots]
+        all_y = [point.y for point in self._dots]
         
         axes = plt.gca()
         axes.set_aspect('equal')
@@ -86,27 +98,22 @@ class Paper():
         axes.scatter(all_x, all_y, marker="o", s=50)
         plt.show()
     
-    def fold(self, instruction: list[str]):
+    def fold(self, instruction: Instruction):
         """ Fold along a given axis.  Returns the union set of 
-        numbers before the fold line, and the flip of the numbers after the fold line.
-
-        Args:
-            instruction (list[str]): x (vertical) or y (horizontal), value of fold axis
-        """
-        instr, axis = instruction[0], int(instruction[1])
-        assert instr in ('x', 'y'), "Instruction must be 'x' or 'y'"
+        numbers before the fold line, and the flip of the numbers after the fold line. """
+        assert instruction.axis in ('x', 'y'), "Instruction must be 'x' or 'y'"
         
         before_fold = set()
         after_fold = set()
         
-        if instr == 'x':    # fold vertical
-            before_fold = set(dot for dot in self._dots if dot.real < axis)
-            after_fold = set(num for num in self._dots if num.real > axis)
-            folded = set(complex(axis-(num.real-axis), num.imag) for num in after_fold)
+        if instruction.axis == 'x':    # fold vertical
+            before_fold = set(dot for dot in self._dots if dot.x < instruction.val)
+            after_fold = set(dot for dot in self._dots if dot.x > instruction.val)
+            folded = set(Point(instruction.val-(num.x-instruction.val), num.y) for num in after_fold)
         else:   # fold horizontal
-            before_fold = set(num for num in self._dots if num.imag < axis)
-            after_fold = set(num for num in self._dots if num.imag > axis)
-            folded = set(complex(num.real, axis-(num.imag-axis)) for num in after_fold)
+            before_fold = set(dot for dot in self._dots if dot.y < instruction.val)
+            after_fold = set(dot for dot in self._dots if dot.y > instruction.val)
+            folded = set(Point(num.x, instruction.val-(num.y-instruction.val)) for num in after_fold)
         
         self._dots = before_fold | folded   
 
@@ -129,18 +136,20 @@ def main():
     logger.info("Part 2 decoded:\n%s", paper)
     paper.render_as_plt()
 
-def process_data(data: str) -> tuple[set[complex], list]:
+def process_data(data: str) -> tuple[set[Point], list]:
     """ Input has n rows of x,y coords, then an empty line, then rows of instructions """
     
-    coords, _, instructions = data.partition('\n\n')
+    coords, _, instruction_lines = data.partition('\n\n')
     dots = set()
     for coord in coords.splitlines():    # e.g. [6, 10]
         x,y = map(int, coord.split(","))
-        dots.add(complex(x, y))
+        dots.add(Point(x, y))
+    
+    instructions = []
+    for line in instruction_lines.splitlines():
+        instr = line.replace("fold along ", "").split("=")
+        instructions.append(Instruction(instr[0], int(instr[1])))
         
-    # convert "fold along y=7" to "[y,7]"
-    instructions = [instruction.replace("fold along ", "").split("=")
-                    for instruction in instructions.splitlines()]
     return dots, instructions
     
 if __name__ == "__main__":
