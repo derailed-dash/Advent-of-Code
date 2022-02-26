2f3ecca6f7bc696672d60d0d5d76d9d5dd0eaf25
Dazbo <derailed.dash@gmail.com>
2022-02-26 23:36:26 +0000

    Started day 2 doc

--- src/AoC_2015/d02_simple_box_geometry/wrapping_paper.py
+++ src/AoC_2015/d02_simple_box_geometry/wrapping_paper.py
@@ -13,6 +13,7 @@ Part 2:
     Ribbon required is shortest distance around sides, i.e. smallest perimeter of any face
         + bow length b, where b = volume of box.
 """
+from dataclasses import dataclass
 import os
 import time
 import re
@@ -21,6 +22,28 @@ SCRIPT_DIR = os.path.dirname(__file__)
 INPUT_FILE = "input/input.txt"
 SAMPLE_INPUT_FILE = "input/sample_input.txt"
 
+@dataclass
+class Box():
+    """ Cuboid """
+    width: int
+    height: int
+    length: int
+    
+    def __init__(self, dims: list) -> None:
+        sorted_dims = sorted(dims)
+        self.width = sorted_dims[0]
+        self.height = sorted_dims[1]
+        self.length = sorted_dims[2]
+    
+    @property
+    def area(self):
+        return 2*(self.width*self.height + self.width*self.length + self.height*self.length)
+    
+    @property
+    def contingency(self):
+        """ Contigency is the same as the area of the smallest face """
+        return self.width * self.height
+    
 
 def main():
     # input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
@@ -36,40 +59,23 @@ def main():
     ribbon = sum(ribbon_required(box) for box in boxes)
     print(f"Ribbon required: {ribbon}")
 
+def paper_required(box: Box):
+    return box.area + box.contingency
 
-def paper_required(box):
-    w = box[0]
-    h = box[1]
-    l = box[2]
-
-    box_area = 2*(w*h+w*l+h*l)
-    contingency_area = w*h
-
-    return box_area + contingency_area
-
-
-def ribbon_required(box):
-    w = box[0]
-    h = box[1]
-    l = box[2]
-
-    ribbon_length = 2*w + 2*h
-    bow_length = w*h*l
+def ribbon_required(box: Box):
+    ribbon_length = 2*box.width + 2*box.height
+    bow_length = box.width*box.height*box.length
 
     return ribbon_length + bow_length
 
-
-def get_boxes(data):
+def get_boxes(data) -> list[Box]:
     boxes = []
 
     p = re.compile(r"(\d+)x(\d+)x(\d+)")
     for line in data:
-        # get list of dimensions
-        dims = p.match(line).groups()
-        # convert dims to int
-        dims = list(map(int, dims))
-        # sort and add to our boxes list
-        boxes.append(sorted(dims))
+        if match := p.match(line):
+            dims = list(map(int, match.groups())) # dims as list of int
+            boxes.append(Box(dims))
 
     return boxes
 
