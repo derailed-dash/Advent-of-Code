b8c0bf3f5672960140bab1f8626080267d0edfca
derailed-dash <derailed.dash@gmail.com>
2022-03-24 20:48:21 +0000

    Day 5 documented

--- src/AoC_2015/d05_regex_string_matching/naughty_or_nice_all_regex.py
+++ src/AoC_2015/d05_regex_string_matching/naughty_or_nice_all_regex.py
@@ -18,48 +18,47 @@ Nice includes ALL of:
     Any two chars that repeat anywhere in the remaining string.
     Any two chars that repeat, with a single char between them.
 """
-import os
+from pathlib import Path
 import time
 import re
 
-SCRIPT_DIR = os.path.dirname(__file__) 
-INPUT_FILE = "input/input.txt"
-SAMPLE_INPUT_FILE = "input/sample_input.txt"
+SCRIPT_DIR = Path(__file__).parent 
+INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
+# INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
 
 # part 1 rules
-three_vowels_match = re.compile(r"([aeiou].*){3}")
-# \1 means: repeat what we matched before
-double_chars_match = re.compile(r"(.)\1")
+three_vowels_match = re.compile(r"([aeiou].*){3,}") # Match any vowel, three or more times
+double_chars_match = re.compile(r"(.)\1")  # \1 means: repeat what we matched before
 bad_chars_match = re.compile(r"ab|cd|pq|xy")
 
 # part 2 rules
 char_pair_repeats_match = re.compile(r"(..).*\1")
 xwx_match = re.compile(r"(.).\1")
 
-
 def main():
-    # input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
-    input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
-    with open(input_file, mode="rt") as f:
+    with open(INPUT_FILE, mode="rt") as f:
         data = f.read().splitlines()
 
-    nice_lines_count = sum(1 for line in data if part_1_rules_match(line))
+    nice_lines_count = [part_1_rules_match(line) for line in data].count(True)
     print(f"Part 1: there are {nice_lines_count} nice strings.")
 
-    nice_lines_count = sum(1 for line in data if part_2_rules_match(line))
+    nice_lines_count = [part_2_rules_match(line) for line in data].count(True)
     print(f"Part 2: there are {nice_lines_count} nice strings.")
 
+def part_2_rules_match(input_line):
+    if (char_pair_repeats_match.search(input_line)
+            and xwx_match.search(input_line)):
+        return True
+    
+    return False
 
-def part_2_rules_match(input):
-    return (char_pair_repeats_match.search(input)
-        and xwx_match.search(input))
-
-
-def part_1_rules_match(input):
-    return (three_vowels_match.search(input) 
-        and double_chars_match.search(input) 
-        and not bad_chars_match.search(input))
-
+def part_1_rules_match(input_line):
+    if (three_vowels_match.search(input_line) 
+            and double_chars_match.search(input_line) 
+            and not bad_chars_match.search(input_line)):
+        return True
+    
+    return False
 
 if __name__ == "__main__":
     t1 = time.perf_counter()
