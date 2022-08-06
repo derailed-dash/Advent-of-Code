734b10016cd09f3eeeedb54a20a95dc9ea818025
derailed-dash <derailed.dash@gmail.com>
2022-08-06 21:58:27 +0100

    Refactor PEG for 2015 day 7

--- src/AoC_2015/d07_PEG_bitwise_logic_circuits/bitwise_logic_circuits_with_peg.py
+++ src/AoC_2015/d07_PEG_bitwise_logic_circuits/bitwise_logic_circuits_with_peg.py
@@ -34,18 +34,24 @@ Part 2:
     Use regex to get the index of the only instruction that ends "-> b" and replace the instruction in the data.
     Then re-run the parse.
 """
+import logging
 import os
 import time
 import re
 from functools import reduce
 from parsimonious import Grammar, NodeVisitor, ParseError, VisitationError
 
+logging.basicConfig(format="%(asctime)s.%(msecs)03d:%(levelname)s:%(name)s:\t%(message)s", 
+                    datefmt='%H:%M:%S')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 SCRIPT_DIR = os.path.dirname(__file__) 
 INPUT_FILE = "input/input.txt"
 SAMPLE_INPUT_FILE = "input/sample_input.txt"
 
 # define the grammar rules
-# EXPR matches any whole line
+# EXPR matches any whole line, e.g. 'k AND m -> n'
 # Note, wires can be one or two chars in name, e.g. a, aa, xy.
 grammar = Grammar(r"""
     expr = input? (op input)? feeds wire
@@ -60,20 +66,31 @@ grammar = Grammar(r"""
 class BitwiseLogicVisitor(NodeVisitor):
     """ PEG Parser for processing the Bitwise instructions """
     # override the parse method, to initialise instance variables and perform the bitwise logic
-    def parse(self, *args, **kwargs):
-        ''' First arg is the string to be parsed
-            Second arg is expected to be a dict that maps wire names to wire values
-        '''
-        self._wires_dict = args[1]
-        self._inputs = []
-        self._op = ""
-        self._target_wire = ""
-        self._processing_input = True
-        self._output = {}
+    def parse(self, *args):
+        """
+        Arguments
+            args[0] - the str to be parsed. E.g. 'k AND m -> n'
+            args[1] - a dict of known wire values 
 
-        # call the super.  Note that it errors if you try to pass in the dict we received.
-        # That's why we're only passing through the first arg, i.e. the string to be parsed.
-        super().parse(args[0], **kwargs)
+        Returns:
+            [dict] - output wire:value
+            
+        Raises:
+            ParseError, VisitationError
+        """
+        
+        self._wires_dict = args[1]
+        
+        # These instance variables are updated after we parse the input line
+        self._inputs = []             # store int input values, left of the '->'. E.g. [7102, 65023]
+        self._op = ""                 # E.g. AND
+        self._target_wire = ""        # E.g. n
+        self._processing_input = True # Set to False after we process the '->'
+        self._output = {}             # Initialise empty wire:value dict
+
+        # Parse out input line, calling our visit_xxx methods
+        # This will update our instance variables
+        super().parse(args[0])  
 
         # perform bitwise operation on the values in the _inputs list
         if "AND" in self._op:
@@ -93,41 +110,45 @@ class BitwiseLogicVisitor(NodeVisitor):
             res = sum(self._inputs)
 
         self._output[self._target_wire] = res
-        # print(f"Inputs were: {self._inputs}, op was: {self._op}, result: {self._output}")   
+        # logger.debug("Inputs were: %s, op was: %s, result: %s", self._inputs, self._op, self._output)   
 
         # Wire name and wire value, as dict
         return self._output
 
     def visit_expr(self, node, visited_children):
         # here we can print the overall expr being parsed
-        # print(f"EXPR Node: {node}; visited_children: {visited_children}")
-        pass
-
-    def visit_input(self, node, visited_children):
+        # logger.debug("EXPR Node:\n%s\nVisited_children: %s", node, visited_children)
         pass
 
     def visit_feeds(self, node, visited_children):
-        # change state so that the next WIRE we parse is treated as output
+        """ Handle '-> '
+        Change state so that the next wire  we parse is treated as output
+        """
         self._processing_input = False
 
     def visit_op(self, node, visited_children):
+        """ Handle "AND" / "OR" / "LSHIFT" / "RSHIFT" / "NOT"
+        """
         self._op = node.text.strip()       
         return self._op
 
     def visit_number(self, node, visited_children):
+        """ A numeric input value """
         number = int(node.text)
         self._inputs.append(number)
         return number
 
     def visit_wire(self, node, visited_children):
-        # a wire is always passed as a str designation.
-
+        """ Handle ~r"[a-z]{1,2}"
+        A wire is always passed as a str designation. E.g. 'lf'
+        Use the _wires_dict to get the numeric value for this wire.
+        If we don't have a value, this will result in a KeyError, 
+        which will be caught and thrown as a VisitationError. """
         wire = node.text.strip()
         if (self._processing_input):
-            # if we have an input wire, then it must have a value stored in the dict
+            # if we have an input wire, then try to extract its numeric value
             self._inputs.append(self._wires_dict[wire])
-        else:
-            # otherwise, this is an output wire
+        else:  # otherwise, this is an output wire
             self._target_wire = wire
 
         return wire
@@ -148,7 +169,7 @@ def main():
     # Pass in a copy of the input data, as we'll need to parse it again for Part 2
     results = process_instructions(data.copy(), blc_visitor)
     a_val = results['a']
-    print(f"Part 1: Value of input a is {a_val}")
+    logger.info("Part 1: Value of input a is %s", a_val)
 
     # Part 2
     wire_b_instr = list(filter(re.compile(r"-> b$").search, data))
@@ -156,34 +177,29 @@ def main():
     data[wire_b_instr_index] = f"{a_val} -> b"
 
     results = process_instructions(data.copy(), blc_visitor)
-    print(f"Part 2: Value of input a is {results['a']}")
+    logger.info("Part 2: Value of input a is %s", results['a'])
 
-def process_instructions(data, blc_visitor):
-    results = {}
+def process_instructions(instructions, blc_visitor):
+    wire_values = {}
 
     # treat all our input as a stack.  
     # Some input values will not be known yet, so park these instructions and try on the next iteration
-    while data:
-        not_ready_to_parse = []
-
-        for i, line in enumerate(data):
-            # print(blc_visitor.grammar.parse(line))
+    while instructions:
+        for i, line in enumerate(instructions):
             try:
-                results.update(blc_visitor.parse(line, results))
+                wire_values.update(blc_visitor.parse(line, wire_values))
                 # if we're here, the instruction parsed successfully, so remove it from the stack permanently
-                data.pop(i)
-            except (ParseError, VisitationError, KeyError):
-                # if the parser tries to retrieve a wire value that is not yet known
-                # a KeyError is thrown, caught, and rethrown as a VisitationError, which we catch here
-                # add this instruction to the list we can't yet parse, and move on to the next instruction
-                not_ready_to_parse.append(data.pop(i))
+                popped = instructions.pop(i)
+                logger.debug("Processed: %s", popped)
+            except (ParseError, VisitationError):
+                # If the parser tries to retrieve a wire value that is not yet known
+                # a KeyError is thrown, caught, and rethrown as a VisitationError, which we catch here.
+                # This instruction can't be processed yet, as we don't have all necessary input values
                 continue
 
-        # We're ready to process the list again.  
-        # Add back in all the instructions we failed to parse last time
-        data.extend(not_ready_to_parse)
+        # We're ready to process the list again. 
     
-    return results
+    return wire_values
 
 if __name__ == "__main__":
     t1 = time.perf_counter()
