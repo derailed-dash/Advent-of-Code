3e839048eb1825e2a2505d21d8d9c1b92f3b28be
derailed-dash <derailed.dash@gmail.com>
2022-08-18 22:59:27 +0100

    Day 2015 d8 underway

--- src/AoC_2015/d08_escaping_and_evaluating_literals/parsing_literals.py
+++ src/AoC_2015/d08_escaping_and_evaluating_literals/parsing_literals.py
@@ -15,20 +15,17 @@ Part 2:
     I.e. escape all chars that need escaping, and wrap in additional quotes.
     E.g. "aaa\"aaa" becomes \"aaa\\\"aaa\", all wrapped in another pair of quotes
 """
-import os
+from pathlib import Path
 import time
 import re
 from ast import literal_eval
 
-SCRIPT_DIR = os.path.dirname(__file__) 
-INPUT_FILE = "input/input.txt"
-SAMPLE_INPUT_FILE = "input/sample_input.txt"
-
+SCRIPT_DIR = Path(__file__).parent 
+INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
+# INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
 
 def main():
-    # input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
-    input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
-    with open(input_file, mode="rt") as f:
+    with open(INPUT_FILE, mode="rt") as f:
         data = f.read().splitlines()
 
     raw_lengths = []
@@ -37,10 +34,12 @@ def main():
     for line in data:
         line = line.strip()
         raw_lengths.append(len(line))
+        
+        # Use literal_eval to take the raw string, and evaluate it as a Python expression
+        str_repr = literal_eval(line)
 
         # Part 1
-        # Use eval to take the raw string, and evaluate it as a Python expression
-        evaluated_lengths.append(len(literal_eval(line)))
+        evaluated_lengths.append(len(str_repr))
 
         # Part 2
         # replace any " with \" and any \ with \\
@@ -57,7 +56,6 @@ def main():
 
     print(f"Sum of encoded strings: {sum_encoded_lengths}") 
     print(f"Diff: {sum_encoded_lengths-sum_raw_lengths}") 
-
 
 if __name__ == "__main__":
     t1 = time.perf_counter()
