0533b14f0c5b2e4f19ff1e7f3d215bfadaf3539a
Dazbo <derailed.dash@gmail.com>
2022-11-27 16:03:48 +0000

    Completed 2015d14 doc

--- src/AoC_2015/d14_reindeer_racing_with_namedtuples/reindeer_racing.py
+++ src/AoC_2015/d14_reindeer_racing_with_namedtuples/reindeer_racing.py
@@ -18,18 +18,37 @@ Part 2:
     Use a defaultdict(int) to store the points.
     Determine which reindeer has the most points at t=2503.
 """
+from dataclasses import dataclass
 import os
 import re
 import time
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 
 SCRIPT_DIR = os.path.dirname(__file__) 
 INPUT_FILE = "input/input.txt"
 SAMPLE_INPUT_FILE = "input/sample_input.txt"
 
-# make our reindeer easier to work with
-reindeer = namedtuple("reindeer", "name speed duration rest_time")
+TIME = 2503
 
+@dataclass
+class Reindeer():
+    """ A reindeer flies at *speed* (km/s) for a given *duration* (s), then must *rest* (s). """
+    name: str
+    speed: int
+    duration: int
+    rest: int
+    
+    def cycle_time(self) -> int:
+        return self.duration + self.rest
+    
+    def distance_at_t(self, t: int) -> int:
+        """Computes the distance travelled by this reindeer in the time given """
+        total_cycles = t // self.cycle_time()
+        remainder = t % self.cycle_time()
+        remainder_travelling = min(remainder, self.duration)
+
+        return ((self.duration*total_cycles) + remainder_travelling) * self.speed
+    
 def main():
     # input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
     input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
@@ -38,77 +57,39 @@ def main():
 
     reindeer_list = process_reindeer_data(data)
 
-    time = 2503
-
-    # Part 1
-    reindeer_distances = {}
-    a_reindeer: reindeer
-    for a_reindeer in reindeer_list:
-        reindeer_distances[a_reindeer.name] = distance_travelled(a_reindeer, time)
-
-    print("Distances: ")
-    print(reindeer_distances)
-    winner_on_distance = max(reindeer_distances.items(), key=lambda x: x[1])[0]
-    print(f"Winning reindeer: {winner_on_distance} with distance of {reindeer_distances[winner_on_distance]}.")
-
-    # Part 2
+    print("Part 1")
+    winner = max(reindeer_list, key=lambda x: x.distance_at_t(TIME))
+    print(f"Winning reindeer: {winner.name} with distance of {winner.distance_at_t(TIME)}.")
+    
+    print("\nPart 2")
     reindeer_points = defaultdict(int)
 
     # we need to determine the winner for each second that has elapsed, 
     # and allocate a point to that reindeer
-    for i in range(1, time+1):
-        for a_reindeer in reindeer_list:
-            reindeer_distances[a_reindeer.name] = distance_travelled(a_reindeer, i)
-
-        current_winner = max(reindeer_distances.items(), key=lambda x: x[1])[0]
-        reindeer_points[current_winner] += 1
+    for i in range(1, TIME+1):
+        current_winner = max(reindeer_list, key=lambda x: x.distance_at_t(i))
+        reindeer_points[current_winner.name] += 1
     
-    print("\nPoints:")
+    print("Points:")
     for a_reindeer in reindeer_points:
         print(f"{a_reindeer}: {reindeer_points[a_reindeer]}")
 
     winner_on_points = max(reindeer_points.items(), key=lambda x: x[1])[0]
     print(f"Winning reindeer: {winner_on_points} who has {reindeer_points[winner_on_points]} points.")
 
+def process_reindeer_data(data) -> list[Reindeer]:
+    """Process input lines, and return a dict of reindeer.
+    Each line is of the format:
+        "Vixen can fly 8 km/s for 8 seconds, but then must rest for 53 seconds."
 
-def distance_travelled(a_reindeer: reindeer, time: int) -> int:
-    """Computes the distance travelled by this reindeer in the time given
-
-    Args:
-        a_reindeer (reindeer):
-        time (int): in seconds
-
-    Returns:
-        [int]
-    """
-    fulL_repeat = get_full_repeat(a_reindeer)
-    total_repeats = time // fulL_repeat
-    remainder = time % fulL_repeat
-    remainder_travelling = min(remainder, a_reindeer.duration)
-
-    return ((a_reindeer.duration*total_repeats) + remainder_travelling) * a_reindeer.speed
-
-
-def get_full_repeat(a_reindeer: reindeer) -> int:
-    return a_reindeer.duration + a_reindeer.rest_time
-
-
-def process_reindeer_data(data) -> list:
-    """Process input lines, and return a dict of reindeer
-
-    Args:
-        data : list of str, where each line is of the format:
-            "Vixen can fly 8 km/s for 8 seconds, but then must rest for 53 seconds."
-
-    Returns:
-        [dict]: K is reindeer name, V is tuple of speed, duration, rest_time
+    Returns: [list]: Reindeers
     """    
     reindeer_pattern = re.compile(r"^(\w+) can fly (\d+) km/s for (\d+) seconds, but then must rest for (\d+) seconds.")
     
     reindeer_list = []
     for line in data:
-        reindeer_name, speed, duration, rest_time = reindeer_pattern.match(line).groups()
-        reindeer_list.append(reindeer(reindeer_name, int(speed), int(duration), int(rest_time)))
+        reindeer_name, speed, duration, rest = reindeer_pattern.findall(line)[0]
+        reindeer_list.append(Reindeer(reindeer_name, int(speed), int(duration), int(rest)))
 
     return reindeer_list
 
@@ -116,4 +97,4 @@ if __name__ == "__main__":
     t1 = time.perf_counter()
     main()
     t2 = time.perf_counter()
-    print(f"Execution time: {t2 - t1:0.4f} seconds")
+    print(f"\nExecution time: {t2 - t1:0.4f} seconds")
