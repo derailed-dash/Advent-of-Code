7f106765c090dda69cc32c0a3571680633ce7f25
Dazbo <derailed.dash@gmail.com>
2022-12-02 09:27:49 +0000

    d02 - business logic extracted to dict

--- src/AoC_2022/d02_rock_paper_scissors/rock_paper_scissors.py
+++ src/AoC_2022/d02_rock_paper_scissors/rock_paper_scissors.py
@@ -18,12 +18,15 @@ Get our total score.
 - Treat first column as what they play. A=Rock, B=Paper, C=Scissors
 - Treat second column as what we play. X=Rock, Y=Paper, Z=Scissors
 
+We abstract the business rules into a dictionary, to avoid lots of if...elif...else.
+
 Part 2:
 
 Get our total score.
 - Treat first column as what they play. A=Rock, B=Paper, C=Scissors
 - Treat second column as outcome required. X=Lose, Y=Draw, Z=Win
 
+We just need to add a few more business rules to the dictionary.
 """
 from pathlib import Path
 import time
@@ -35,12 +38,9 @@ HAND = {
 }
 
 SCORES = {
-    "X": 1,
-    "Y": 2,
-    "Z": 3,
-    "ROCK": 1,
-    "PAPER": 2,
-    "SCISSORS": 3,
+    HAND["ROCK"]: 1,
+    HAND["PAPER"]: 2,
+    HAND["SCISSORS"]: 3,
     "LOSE": 0,
     "DRAW": 3,
     "WIN": 6
@@ -50,6 +50,30 @@ REQUIRED = {
     "LOSE": "X",
     "DRAW": "Y",
     "WIN": "Z"
+}
+
+RULES = { 
+    # (Their hand, my hand): score
+    (HAND["ROCK"], HAND["SCISSORS"]): SCORES["LOSE"],
+    (HAND["ROCK"], HAND["ROCK"]): SCORES["DRAW"],
+    (HAND["ROCK"], HAND["PAPER"]): SCORES["WIN"],
+    (HAND["PAPER"], HAND["ROCK"]): SCORES["LOSE"],
+    (HAND["PAPER"], HAND["PAPER"]): SCORES["DRAW"],
+    (HAND["PAPER"], HAND["SCISSORS"]): SCORES["WIN"],
+    (HAND["SCISSORS"], HAND["PAPER"]): SCORES["LOSE"],
+    (HAND["SCISSORS"], HAND["SCISSORS"]): SCORES["DRAW"],
+    (HAND["SCISSORS"], HAND["ROCK"]): SCORES["WIN"],
+    
+    # (Their hand, my goal): score-for-my-hand
+    (HAND["ROCK"], REQUIRED["LOSE"]): SCORES[HAND["SCISSORS"]],
+    (HAND["ROCK"], REQUIRED["DRAW"]): SCORES[HAND["ROCK"]],
+    (HAND["ROCK"], REQUIRED["WIN"]): SCORES[HAND["PAPER"]],
+    (HAND["PAPER"], REQUIRED["LOSE"]): SCORES[HAND["ROCK"]],
+    (HAND["PAPER"], REQUIRED["DRAW"]): SCORES[HAND["PAPER"]],
+    (HAND["PAPER"], REQUIRED["WIN"]): SCORES[HAND["SCISSORS"]],
+    (HAND["SCISSORS"], REQUIRED["LOSE"]): SCORES[HAND["PAPER"]],
+    (HAND["SCISSORS"], REQUIRED["DRAW"]): SCORES[HAND["SCISSORS"]],
+    (HAND["SCISSORS"], REQUIRED["WIN"]): SCORES[HAND["ROCK"]] 
 }
 
 SCRIPT_DIR = Path(__file__).parent
@@ -68,73 +92,37 @@ def main():
     scores = play_part_2(rounds)
     print(f"Part 2: Your score = {sum(scores)}")
 
+def play_part_1(rounds: list):
+    """ Here we treat X, Y, Z as whether we play Rock, Paper, Scissors, respectively. 
+    Calculate our score for each hand based on what hand we play, and whether we win, lose or draw. """
+    scores = []
+    zyz_to_rps = {"X": HAND["ROCK"], "Y": HAND["PAPER"], "Z": HAND["SCISSORS"]}
+    for this_round in rounds:
+        their_hand, my_hand = this_round
+        my_hand = zyz_to_rps[my_hand]
+        
+        this_score = 0
+        this_score += SCORES[my_hand]
+        this_score += RULES[(their_hand, my_hand)]    
+        scores.append(this_score)
+    
+    return scores        
+
 def play_part_2(rounds: list):
     """ Here we treat X, Y, Z as whether we play Lose, Draw, or Win, respectively. 
     Determine what hand we should play to achieve that outcome.
     Then calculate our score for each hand based on what hand we chose, and whether we win, lose or draw. """
     scores = []
+    xyz_to_ldw = {"X": SCORES["LOSE"], "Y": SCORES["DRAW"], "Z": SCORES["WIN"]}
     for this_round in rounds:
+        their_hand, my_goal = this_round
         this_score = 0
-
-        if this_round[1] == REQUIRED["LOSE"]:
-            this_score += SCORES["LOSE"]
-            
-            if this_round[0] == HAND["PAPER"]:
-                this_score += SCORES["ROCK"]
-            elif this_round[0] == HAND["ROCK"]:
-                this_score += SCORES["SCISSORS"]
-            else:
-                this_score += SCORES["PAPER"]
-        elif this_round[1] == REQUIRED["DRAW"]:
-            this_score += SCORES["DRAW"]
-            
-            if this_round[0] == HAND["PAPER"]:
-                this_score += SCORES["PAPER"]
-            elif this_round[0] == HAND["ROCK"]:
-                this_score += SCORES["ROCK"]
-            else:
-                this_score += SCORES["SCISSORS"]
-        else: # I must win
-            this_score += SCORES["WIN"]
-            
-            if this_round[0] == HAND["PAPER"]:
-                this_score += SCORES["SCISSORS"]
-            elif this_round[0] == HAND["ROCK"]:
-                this_score += SCORES["PAPER"]
-            else:
-                this_score += SCORES["ROCK"]
+        this_score += xyz_to_ldw[my_goal]
+        this_score += RULES[(their_hand, my_goal)]        
     
         scores.append(this_score)
         
-    return scores           
-    
-def play_part_1(rounds: list):
-    """ Here we treat X, Y, Z as whether we play Rock, Paper, Scissors, respectively. 
-    Calculate our score for each hand based on what hand we play, and whether we win, lose or draw. """
-    scores = []
-    for this_round in rounds:
-        this_score = 0
-        this_score += SCORES[this_round[1]]
-        
-        if this_round[1] == "X": # I play rock
-            if this_round[0] == HAND["ROCK"]: # they play rock
-                this_score += SCORES["DRAW"]
-            elif this_round[0] == HAND["SCISSORS"]: # they play scissors
-                this_score += SCORES["WIN"]
-        elif this_round[1] == "Y": # I play paper
-            if this_round[0] == HAND["PAPER"]: # they play paper
-                this_score += SCORES["DRAW"]
-            elif this_round[0] == HAND["ROCK"]: # they play rock
-                this_score += SCORES["WIN"]
-        else: # I play scissors
-            if this_round[0] == HAND["SCISSORS"]: # they play scissors
-                this_score += SCORES["DRAW"]
-            elif this_round[0] == HAND["PAPER"]: # they play paper
-                this_score += SCORES["WIN"]
-    
-        scores.append(this_score)
-    
-    return scores              
+    return scores      
 
 def play_rounds(data: list[str]) -> list:
     """ Returns a list. Each element in the list is a pair as a list, e.g. ["A", "X"] """
