642d2193f0e09e0e62625dfaead06fe692ae7884
Dazbo <derailed.dash@gmail.com>
2022-12-05 08:10:08 +0000

    2022d05 read config code

--- src/AoC_2022/d05_supply_stacks/supply_stacks.py
+++ src/AoC_2022/d05_supply_stacks/supply_stacks.py
@@ -1,13 +1,40 @@
 """
 Author: Darren
-Date: 01/12/2022
+Date: 05/12/2022
 
-Solving https://adventofcode.com/2022/day/1
+Solving https://adventofcode.com/2022/day/5
+
+We have stacks of crates. First block of input data shows stack configuration, e.g.
+            [D]    
+        [N] [C]    
+        [Z] [M] [P]
+         1   2   3
+
+We have instructions for rearranging crates.  E.g. 
+move 3 from 1 to 3
 
 Part 1:
 
+Our CrateMover 9000 moves one crate at the time, from the end of one stack to the end of another.
+What crates will be at the top of each stack, after following the instructions?
+
+- Solution:
+  - Split the instructions into two blocks: crate config, and instructions.
+    - For config, first reverse the order, count how many stacks, and then parse with regex.
+  - Process each instruction, popping off the end of the source stack as many times as required,
+    and appending to the target stack.
+  
 Part 2:
 
+Our CrateMover 9001 moves multiple crates at a time, 
+and puts them in the new stack in the same order that they were lifted.
+What crates will be at the top of each stack, after following the instructions?
+
+- Solution:
+  - The same, except now we can slice n items off the end of the source stack, 
+    rather than popping one at a time.
+  - Because we're now adding a list (slice) to the target stack, we need to use extend, not append.
+  
 """
 from copy import deepcopy
 from pathlib import Path
@@ -20,20 +47,8 @@ INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
 def main():
     with open(INPUT_FILE, mode="rt") as f:
         stack_data, instructions = f.read().split("\n\n")
-    
-    # Let's just write out the stacks for now, to save time parsing...
-    stacks = [
-        ["G", "T", "R", "W"],
-        ["G", "C", "H", "P", "M", "S", "V", "W"],
-        ["C", "L", "T", "S", "G", "M"],
-        ["J", "H", "D", "M", "W", "R", "F"],
-        ["P", "Q", "L", "H", "S", "W", "F", "J"],
-        ["P", "J", "D", "N", "F", "M", "S"],
-        ["Z", "B", "D", "F", "G", "C", "S", "J"],
-        ["R", "T", "B"],
-        ["H", "N", "W", "L", "C"]
-    ]
-
+   
+    stacks = process_stack_data(stack_data.splitlines())
     movements = read_instructions(instructions.splitlines())
     
     # Part 1
@@ -55,6 +70,35 @@ def main():
         
     stack_message = "".join(a_stack[-1] for a_stack in stacks)
     print(f"Part 2: {stack_message}")        
+
+def process_stack_data(stack_data: list[str]) -> list[list]:
+    """ Data looks like... 
+            [D]    
+        [N] [C]    
+        [Z] [M] [P]
+         1   2   3
+         
+    Return: [['Z', 'N'], ['M', 'C', 'D'], ['P']]
+    """
+    stack_width = 4
+    p = re.compile(r"[A-Z]")
+    # reverse it, so we've got the stack numbers at the top
+    stack_data = stack_data[::-1]
+    num_stacks = len(stack_data[0].split())
+
+    stacks = []
+    for stack_num in range(num_stacks):
+        this_stack = []
+        stacks.append(this_stack)
+        
+    # proces the stacks
+    for stack_row in stack_data[1:]:     # starting at the row of crates
+        for stack_num in range(num_stacks):
+            match = p.search(stack_row[stack_num * stack_width:(stack_num+1) * stack_width])
+            if match:
+                stacks[stack_num].append(match.group())
+        
+    return stacks
 
 def read_instructions(instructions: list[str]) -> list[tuple[int, int, int]]:
     """ Instructions look like: 'move 3 from 8 to 6' """
