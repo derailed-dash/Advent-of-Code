229bc00073b80aa29f0fb6a608975dba018ece8a
derailed-dash <derailed.dash@gmail.com>
2022-12-17 22:55:15 +0000

    Gives wrong answer with test data

--- src/AoC_2022/d17_pyroclastic_flow/tetris.py
+++ src/AoC_2022/d17_pyroclastic_flow/tetris.py
@@ -6,7 +6,6 @@ Solving https://adventofcode.com/2022/day/17
 
 Rocks are falling.  And they resemble tetris pieces! They always fall in this order:
 -, +, backwards Lâ, |, ■.
-Probably a mod problem!
 
 Chamber is 7 units wide.  Rocks start to fall from:
 - left edge 2 units from left wall
@@ -40,11 +39,11 @@ INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
 OUTPUT_FILE = Path(SCRIPT_DIR, "output/output.png")
 
 SHAPES = {
-    "HLINE":  {(0, 0), (1, 0), (2, 0), (3, 0)},
-    "PLUS": {(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)},
-    "BACKWARDS_L": {(0, 0),(1, 0),(2, 0),(2, 1),(2, 2)},
-    "I": {(0, 0), (0, 1), (0, 2), (0, 3)},
-    "SQUARE": {(0, 0), (1, 0), (0, 1), (1, 1)}
+    "HLINE":       {(0, 0), (1, 0), (2, 0), (3, 0)},
+    "PLUS":        {(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)},
+    "BACKWARDS_L": {(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)},
+    "I":           {(0, 0), (0, 1), (0, 2), (0, 3)},
+    "SQUARE":      {(0, 0), (1, 0), (0, 1), (1, 1)}
 }
 
 MOVE = {
@@ -135,7 +134,6 @@ class Tower():
     def drop_shape(self):
         self.current_shape = Shape.create_shape_by_type(
                 self._next_shape(), Point(self._x_origin(), self._y_origin()))
-        print(f"Added {self.current_shape}")
         print(self)
             
         self.shape_falling = True
@@ -168,13 +166,13 @@ class Tower():
         
         # Move the shape; construct a new shape from the new Points
         candidate_points = {(point + Point(*MOVE[direction])) for point in self.current_shape.points}
-        if self._get_at_rest_points() & candidate_points:
-            return False
-        else:
+        if self._get_at_rest_points() & candidate_points: # If the candidate would intersect
+            return False # Then this is not a valid posiiton
+        else: # We can move there. Update our current shape position.
             self.current_shape = Shape.create_shape_from_points(candidate_points)
         return True
                     
-    def _get_at_rest_points(self):
+    def _get_at_rest_points(self) -> set[Point]:
         """ Get all the points from all the at_rest shapes """
         points = set()
         for shape in self._all_at_rest_shapes:
@@ -185,7 +183,7 @@ class Tower():
         rows = []
         all_at_rest_shapes_points = self._get_at_rest_points()
             
-        for y in range(Tower.FLOOR_Y, max(point.y for point in self.current_shape.points) + 1):
+        for y in range(Tower.FLOOR_Y, self.height() + 1):
             line = ""
             if y == Tower.FLOOR_Y:
                 line += "+" + (Tower.FLOOR * Tower.WIDTH) + "+"
@@ -203,10 +201,13 @@ class Tower():
             rows.append(line)
         
         return f"{repr(self)}\n" + "\n".join(rows[::-1]) 
+
+    def height(self) -> int:
+        points = self._get_at_rest_points() | self.current_shape.points
+        return max(point.y for point in points)
     
     def __repr__(self) -> str:
-        return (f"Tower(rested={len(self._all_at_rest_shapes)})")
-
+        return (f"Tower(height={self.height()}, rested={len(self._all_at_rest_shapes)})")
 
 def main():
     with open(INPUT_FILE, mode="rt") as f:
@@ -215,10 +216,10 @@ def main():
     print(data)
     
     tower = Tower(jet_pattern=data)
-    for _ in range(2022):
+    for _ in range(15):
         tower.drop_shape()
     
-    # print the height
+    print(f"Part 1: {repr(tower)}")
 
 if __name__ == "__main__":
     t1 = time.perf_counter()
