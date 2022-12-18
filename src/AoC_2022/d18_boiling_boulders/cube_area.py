1a3c8e3faa7a036535d6f092e823886b8e882d4c
Dazbo <derailed.dash@gmail.com>
2022-12-18 14:23:21 +0000

    Part 2 not quite there yet

--- src/AoC_2022/d18_boiling_boulders/cube_area.py
+++ src/AoC_2022/d18_boiling_boulders/cube_area.py
@@ -28,18 +28,20 @@ Soln:
 - Find all adjacent cubes that are not in our list:
   - These are either:
     - Part of internal pockets. If we flood fill a pocket, it will have a boundary.
-    - Part of path to the outside. If we flood fill, we will reach a cube beyond all adjacents.
+    - Part of path to the outside. If we flood fill, we will reach a cube beyond all adjacents. No close boundary.
 - To solve:
   - Subtract surface area of all internal pockets.
 """
 from __future__ import annotations
+from collections import deque
 from dataclasses import dataclass
+from functools import cache
 from pathlib import Path
 import time
 
 SCRIPT_DIR = Path(__file__).parent
-INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
-# INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
+# INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
+INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
 
 @dataclass(frozen=True)
 class Cube():
@@ -73,8 +75,48 @@ class Droplet():
     def _calculate_values(self): 
         for cube in self.cubes:
             self._all_adjacent_positions |= cube.adjacent()
-            self._adjacent_empty.union(cube for cube in cube.adjacent() if cube not in self.cubes)
+            self._adjacent_empty.update(cube for cube in cube.adjacent() if cube not in self.cubes)
             self.all_surface_area += Droplet.ADJACENT_FACES - len(self.cubes & cube.adjacent())
+    
+    def get_internal_surface_area(self):
+        internal_empty_cubes = self._get_internal_space()
+        internal_surface_area = 0
+        for internal_empty in internal_empty_cubes:
+            internal_surface_area += Droplet.ADJACENT_FACES - len(internal_empty_cubes & internal_empty.adjacent())
+        
+        return internal_surface_area
+    
+    def _get_internal_space(self):
+        """ Process all adjacent empty locations. 
+        Find those that don't connect outside and therefore must be internally bound. """
+        
+        cube_internal = set()  # store all internal empty
+        for cube in self._adjacent_empty:   # loop through all adjacent empty
+            if not self._external_connect(cube):  # if this empty doesn't connect to outside, it's internal
+                cube_internal.add(cube)
+
+        print(f"Internal empty: {cube_internal}")                
+        return cube_internal
+    
+    def _external_connect(self, cube: Cube) -> bool:
+        frontier = deque([cube])
+        explored = {cube}
+        
+        while frontier:
+            current_cube = frontier.popleft() # FIFO for BFS
+            
+            # our goal is when this point seems to have no bound
+            if len(explored) > 5000:
+                return True
+            
+            # We want to look at all neighbours of this empty space, but only if they are also empty
+            for neighbour in current_cube.adjacent():
+                if neighbour not in self.cubes:
+                    if neighbour not in explored:
+                        explored.add(neighbour)
+                        frontier.append(neighbour)
+                    
+        return False
     
 def main():
     with open(INPUT_FILE, mode="rt") as f:
@@ -83,11 +125,11 @@ def main():
     droplet = Droplet(parse_cubes(data))
     
     # Part 1
-    print(f"Part 1: surface_area={droplet.all_surface_area}")
+    print(f"Part 1: all surface area={droplet.all_surface_area}")
 
     # Part 2
-
-        
+    internal = droplet.get_internal_surface_area()
+    print(f"Part 2: external surface area={droplet.all_surface_area - internal}")
     
 def parse_cubes(data: list[str]) -> set[Cube]:
     cubes = set()
