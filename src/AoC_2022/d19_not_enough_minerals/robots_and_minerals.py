01906b478b31d5483f94bc649469681a2bee64dd
derailed-dash <derailed.dash@gmail.com>
2022-12-20 01:43:22 +0000

    2022d19 code done

--- src/AoC_2022/d19_not_enough_minerals/robots_and_minerals.py
+++ src/AoC_2022/d19_not_enough_minerals/robots_and_minerals.py
@@ -29,91 +29,100 @@ Soln:
 - Then just BFS through all possible states. Return the one with the most geodes.
 
 Part 2:
-
+- As above, but with more minutes.
 """
-from __future__ import annotations
 from dataclasses import dataclass
 from pathlib import Path
+from collections import deque
 import re
 import time
 
 SCRIPT_DIR = Path(__file__).parent
-INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
-# INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
-OUTPUT_FILE = Path(SCRIPT_DIR, "output/output.png")
+INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
+# INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
 
 @dataclass
 class Blueprint():
     id: int
-    costs: dict # { "ore": {"ore": 4}, "obsidian": {"ore": 3, clay: 14}, ...}    
-
+    costs: dict # { "ore": {"ore": 4}, "obsidian": {"ore": 3, clay: 14}, ...}
+    
+    def get_max_ore_cost(self):
+        return max([self.costs["ore"]["ore"], 
+                    self.costs["clay"]["ore"], 
+                    self.costs["obsidian"]["ore"], 
+                    self.costs["geode"]["ore"]])
+ 
 @dataclass(frozen=True)
 class State():
-    blueprint: Blueprint
-    time_remaining: int
+    t_remaining: int
     
-    geode_robots: int = 0
-    obsidian_robots: int = 0
-    clay_robots: int = 0
-    ore_robots: int = 1
-    
-    geode: int = 0
-    obsidian: int = 0
+    ore: int = 0   
     clay: int = 0
-    ore: int = 0
+    obsidian: int = 0
+    geode: int = 0
+
+    ore_r: int = 1
+    clay_r: int = 0
+    obsidian_r: int = 0
+    geode_r: int = 0
     
-    def next_state(self) -> State:
-        """ Generator that returns next state. There are only five next states:
-        1: Build nothing (accumulate) 
-        2-5: Build a robot, if you can """
-        time_remaining = self.time_remaining-1
-        
-        # each robot collects one of its resources per minute
-        geode = self.geode + self.geode_robots
-        obsidian = self.obsidian + self.obsidian_robots
-        clay = self.clay + self.clay_robots
-        ore = self.ore + self.ore_robots
-        
-        # do nothing        
-        yield State(self.blueprint, time_remaining, 
-                    self.geode_robots, self.obsidian_robots, self.clay_robots, self.ore_robots,
-                    geode, obsidian, clay, ore)
-        
-        # build a geode robot
-        if (self.obsidian >= self.blueprint.costs["geode"]["obsidian"] 
-                and  self.ore >= self.blueprint.costs["geode"]["ore"]):
-            yield State(self.blueprint, time_remaining, 
-                        self.geode_robots+1, self.obsidian_robots, self.clay_robots, self.ore_robots,
-                        geode, 
-                        obsidian - self.blueprint.costs["geode"]["obsidian"],
-                        clay,
-                        ore - self.blueprint.costs["geode"]["ore"])
-        
-        # build obsidian robot
-        if (self.ore >= self.blueprint.costs["obsidian"]["ore"] 
-                and  self.clay >= self.blueprint.costs["obsidian"]["clay"]):
-            yield State(self.blueprint, time_remaining, 
-                        self.geode_robots, self.obsidian_robots+1, self.clay_robots, self.ore_robots,
-                        geode, 
-                        obsidian,
-                        clay - self.blueprint.costs["obsidian"]["clay"],
-                        ore - self.blueprint.costs["obsidian"]["ore"])
+def bfs(blueprint: Blueprint, state: State):
+    best = 0
+    frontier = deque([state])
+    explored = set()
     
-def main():
-    with open(INPUT_FILE, mode="rt") as f:
-        data = f.read().splitlines()
+    while frontier:
+        state = frontier.popleft()
+
+        best = max(best, state.geode)
+        if state.t_remaining == 0:
+            continue
+
+        max_ore_cost = blueprint.get_max_ore_cost()
         
-    blueprints = parse_data(data)
-    
-    for blueprint in blueprints:
-        state = State(blueprint, time_remaining=24)
-        best = bfs(state)
-        print(state)
-        print(f"Best={best}\n")
-    
-def bfs(state: State) -> int:
-    return 0
-    
+        ore_r = min(state.ore_r, max_ore_cost)
+        clay_r = min(state.clay_r, blueprint.costs["obsidian"]["clay"])
+        obs_r = min(state.obsidian_r, blueprint.costs["geode"]["obsidian"])
+            
+        ore = min(state.ore, state.t_remaining * max_ore_cost - ore_r*(state.t_remaining-1))
+        clay = min(state.clay, state.t_remaining * blueprint.costs["obsidian"]["clay"] - clay_r*(state.t_remaining-1))
+        obs = min(state.obsidian, state.t_remaining * blueprint.costs["geode"]["obsidian"] - obs_r*(state.t_remaining-1))
+
+        state = State(state.t_remaining, 
+                 ore, clay, obs, state.geode,
+                 ore_r, clay_r, obs_r, state.geode_r)
+
+        if state in explored:
+            continue
+        explored.add(state)
+        
+        # Don't make any robots; just accumulate
+        frontier.append(State(state.t_remaining-1, 
+                ore+ore_r, clay+clay_r, obs+obs_r, state.geode+state.geode_r,
+                ore_r, clay_r, obs_r, state.geode_r))
+        
+        if ore >= blueprint.costs["ore"]["ore"]: # build ore robot
+            frontier.append(State(state.t_remaining-1,
+                    ore-blueprint.costs["ore"]["ore"]+ore_r, clay+clay_r, obs+obs_r, state.geode+state.geode_r, 
+                    ore_r+1, clay_r, obs_r, state.geode_r))
+        if ore >= blueprint.costs["clay"]["ore"]: # build clay robot
+            frontier.append(State(state.t_remaining-1, 
+                    ore-blueprint.costs["clay"]["ore"]+ore_r, clay+clay_r, obs+obs_r, state.geode+state.geode_r, 
+                    ore_r, clay_r+1, obs_r, state.geode_r))
+        if ore >= blueprint.costs["obsidian"]["ore"] and clay>=blueprint.costs["obsidian"]["clay"]: # build obsidian robot
+            frontier.append(State(state.t_remaining-1,
+                    ore-blueprint.costs["obsidian"]["ore"]+ore_r, 
+                    clay-blueprint.costs["obsidian"]["clay"]+clay_r, 
+                    obs+obs_r, state.geode+state.geode_r, 
+                    ore_r, clay_r, obs_r+1, state.geode_r))
+        if ore >= blueprint.costs["geode"]["ore"] and obs>=blueprint.costs["geode"]["obsidian"]: # build geode robot
+            frontier.append(State(state.t_remaining-1,
+                    ore-blueprint.costs["geode"]["ore"]+ore_r, clay+clay_r, 
+                    obs-blueprint.costs["geode"]["obsidian"]+obs_r, state.geode+state.geode_r, 
+                    ore_r, clay_r, obs_r, state.geode_r+1))
+            
+    return best
+
 def parse_data(data: list[str]) -> list[Blueprint]:
     """ Read the input and return a list of Blueprint """
     pattern = re.compile(r"Each (\w+) robot costs (\d+) (\w+)(?: and (\d+) (\w+))*.")
@@ -132,8 +141,27 @@ def parse_data(data: list[str]) -> list[Blueprint]:
     
         blueprints.append(Blueprint(blueprint_id, costs))
     
-    return blueprints            
-         
+    return blueprints  
+
+def main():
+    with open(INPUT_FILE, mode="rt") as f:
+        data = f.read().splitlines()
+        
+    blueprints = parse_data(data)
+    quality_level = 0
+    geode_product = 1
+    for blueprint in blueprints:
+        print(blueprint)
+        geodes = bfs(blueprint, State(24))
+        quality_level += geodes * blueprint.id
+        
+        if blueprint.id <= 3:  # First three blueprints; they start at 1
+            geodes = bfs(blueprint, State(32)) # These take a while!
+            geode_product *= geodes
+    
+    print(f"Part 1={quality_level}")
+    print(f"Part 2={geode_product}")                
+    
 if __name__ == "__main__":
     t1 = time.perf_counter()
     main()
