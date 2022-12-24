939c7714e140524cd99035591a4a6fd149083c64
Dazbo <derailed.dash@gmail.com>
2022-12-24 09:37:56 +0000

    2022d24 blizzard movement modelled

---
+++ src/AoC_2022/d24_blizzard_basin/blizzard_navigating.py
@@ -0,0 +1,131 @@
+"""
+Author: Darren
+Date: 24/12/2022
+
+Solving https://adventofcode.com/2022/day/24
+
+We have a map of ground, with walls and blizzards.
+Blizzards move simultaneously, and wrap around.
+We move simultaneously with blizzards. We can move or wait.
+
+Part 1:
+
+What is the fewest number of minutes required to avoid the blizzards and reach the goal?
+
+Soln:
+- Create a MapState.
+- There are no v or ^ blizzards in the cols with openings, so we can ignore this case.
+- Use BFS.
+
+Part 2:
+
+"""
+from __future__ import annotations
+from collections import defaultdict
+from dataclasses import dataclass
+from pathlib import Path
+import time
+
+SCRIPT_DIR = Path(__file__).parent
+INPUT_FILE = Path(SCRIPT_DIR, "input/sample_input.txt")
+# INPUT_FILE = Path(SCRIPT_DIR, "input/input.txt")
+
+@dataclass(frozen=True)
+class Point():
+    """ Point x,y which knows how to add another point, 
+    and how to return all adjacent points, including diagonals. """
+    x: int
+    y: int
+
+    def __add__(self, other) -> Point:
+        """ Subtract other point from this point, returning new point vector """
+        return Point(self.x + other.x, self.y + other.y)
+    
+    def __repr__(self):
+        return f"P({self.x},{self.y})"
+
+class MapState():    
+    def __init__(self, grid_dims: tuple, locations: dict, me_locn: Point) -> None:
+        self._map_locs: dict[Point, set] = locations # { point: { blizzards } }  Can contain a blizzard or a wall
+        self._width, self._height = grid_dims
+        self._me = me_locn
+        self._goal = Point(self._width-2, self._height-1)
+
+    def next_state(self):
+        new_blizzard_locs = defaultdict(set)
+        row_width = self._width - 2 # ignore walls
+        col_height = self._height - 2 # ignore walls
+        
+        for y in range(1, self._height):
+            for x in range(1, self._width):
+                curr_locn = Point(x,y)
+                if curr_locn in self._map_locs: # go through all locations that contain something
+                    for map_locn, contained in self._map_locs.items():
+                        for item in contained: # it could be a '#', or one-or-many blizzards
+                            # Because our grid area excludes row 0 and col 0, subtract 1 from these coords
+                            x = (map_locn.x-1)
+                            y = (map_locn.y-1)
+                            if item is not '#':
+                                # For blizzard, find next position by adding blizzard vector. Handle the wrap around.
+                                x = (x + VECTORS[item].x + row_width) % row_width
+                                y = (y + VECTORS[item].y + col_height) % col_height
+                            
+                            # And now add the +1 back to both coords
+                            new_blizzard_locs[Point(x+1, y+1)].add(item)        
+        
+        return MapState((self._width, self._height), new_blizzard_locs, self._me)        
+        
+    @classmethod
+    def create_from_grid(cls, grid: list[str]):
+        """ Normal route to create an initial MapState from a grid """
+        locations = defaultdict(set)
+        for y, row in enumerate(grid):
+            for x, val in enumerate(row):
+                if val is not ".": 
+                    locations[Point(x,y)].add(val)
+        
+        grid_dims = (len(grid[0]), len(grid))
+        me = Point(1,0)
+        return cls(grid_dims, locations, me)
+        
+    def __str__(self) -> str:
+        lines = []
+        for y in range(0, self._height):
+            line = ""
+            for x in range (0, self._width):
+                curr_locn = Point(x,y)
+                count = len(self._map_locs[curr_locn])
+                if count == 1: # one blizzard here
+                    line += next(val for val in self._map_locs[curr_locn])
+                elif count == 0: # Nothing here
+                    line += "."
+                else: # more than one blizzard here
+                    line += str(len(self._map_locs[curr_locn]))
+                    
+            lines.append(line)
+            
+        return "\n".join(lines) + f"\nCurrent={self._me}, Goal = {self._goal}"
+
+VECTORS = {
+    '^': Point(0, -1),
+    '>': Point(1, 0),
+    'v': Point(0, 1),
+    '<': Point(-1, 0)
+}
+
+def main():
+    with open(INPUT_FILE, mode="rt") as f:
+        data = f.read().splitlines()
+        
+    state = MapState.create_from_grid(data)
+    print(state)
+    
+    for i in range(5):
+        state = state.next_state()
+        print(state)
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main()
+    t2 = time.perf_counter()
+    print(f"Execution time: {t2 - t1:0.4f} seconds")
