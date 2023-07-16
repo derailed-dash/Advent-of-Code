212153537fd11087a2fbc7ed5156b02d76f1498d
Dazbo <darren_lester@epam.com>
2023-07-16 21:51:38 +0100

    2015d21 doc finished

--- src/AoC_2015/d21_boss_fight_classes_namedtuples_combinations_comprehension_if/boss_fight.py
+++ src/AoC_2015/d21_boss_fight_classes_namedtuples_combinations_comprehension_if/boss_fight.py
@@ -282,4 +282,4 @@ if __name__ == "__main__":
     t1 = time.perf_counter()
     main()
     t2 = time.perf_counter()
-    print(f"Execution time: {t2 - t1:0.4f} seconds")
+    logger.info("Execution time: %.3f seconds", t2 - t1)
