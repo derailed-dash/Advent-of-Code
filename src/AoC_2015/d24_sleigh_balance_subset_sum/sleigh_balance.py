e38b7c58a9fcaeaee3dc5b67962ee3b92f44b7d3
derailed-dash <darren_lester@epam.com>
2023-08-13 16:31:37 +0100

    Getting ready for 2015d24 docs

--- src/AoC_2015/d24_sleigh_balance_subset_sum/sleigh_balance.py
+++ src/AoC_2015/d24_sleigh_balance_subset_sum/sleigh_balance.py
@@ -21,48 +21,45 @@ Solution:
 """
 from __future__ import absolute_import
 import logging
-import os
 import time
 from math import prod
 from itertools import combinations
+import common.type_defs as td
+
+locations = td.get_locations(__file__)
+logger = td.retrieve_console_logger(locations.script_name)
+logger.setLevel(logging.INFO)
 
 # pylint: disable=logging-fstring-interpolation
-
-SCRIPT_DIR = os.path.dirname(__file__) 
-INPUT_FILE = "input/input.txt"
-SAMPLE_INPUT_FILE = "input/sample_input.txt"
-
 
 def main():
     logging.basicConfig(level=logging.DEBUG, format="%(asctime)s:%(levelname)s:\t%(message)s")
     
-    # input_file = os.path.join(SCRIPT_DIR, SAMPLE_INPUT_FILE)
-    input_file = os.path.join(SCRIPT_DIR, INPUT_FILE)
-    with open(input_file, mode="rt") as f:
+    # with open(locations.sample_input_file, mode="rt") as f:
+    with open(locations.input_file, mode="rt") as f:
         package_weights = [int(x) for x in f.read().splitlines()]
     
-    logging.info(f"Package weights: {package_weights}")
+    logger.info(f"Package weights: {package_weights}")
 
     # Part 1
     optimum_solution = distribute_packages(package_weights, 3)
-    logging.info(f"Solution found with QE {get_quantum_entanglement(optimum_solution)}")
-    logging.info(f"First bag: {optimum_solution}")
+    logger.info(f"Solution found with QE {get_quantum_entanglement(optimum_solution)}")
+    logger.info(f"First bag: {optimum_solution}")
     
     # Part 2
     optimum_solution = distribute_packages(package_weights, 4)
-    logging.info(f"Solution found with QE {get_quantum_entanglement(optimum_solution)}")
-    logging.info(f"First bag: {optimum_solution}")
-
+    logger.info(f"Solution found with QE {get_quantum_entanglement(optimum_solution)}")
+    logger.info(f"First bag: {optimum_solution}")
 
 def distribute_packages(package_weights, number_of_bags) -> tuple:
-    logging.info(f"Solving for {number_of_bags} bags")
+    logger.info(f"Solving for {number_of_bags} bags")
     
     package_count = len(package_weights)
     total_weight = sum(package_weights)
     target_weight_per_bag = total_weight // number_of_bags
     
-    logging.debug(f"Total packages: {package_count}, with total weight: {total_weight}")
-    logging.debug(f"Target weight per bag: {target_weight_per_bag}")
+    logger.debug(f"Total packages: {package_count}, with total weight: {total_weight}")
+    logger.debug(f"Target weight per bag: {target_weight_per_bag}")
 
     # Get all combos for first bag.
     # Sort by bags in the combo, since the first bag should have fewest packages.    
@@ -91,7 +88,6 @@ def distribute_packages(package_weights, number_of_bags) -> tuple:
 
 def get_quantum_entanglement(bag: tuple):
     return prod(bag)
-
 
 def faster_subset_sum(items: list, target: int, partial=[], results=[]) -> list:
     """
@@ -123,7 +119,6 @@ def faster_subset_sum(items: list, target: int, partial=[], results=[]) -> list:
 
     return results
 
-
 def simple_subset_sum(items, target: int) -> tuple:
     """ Return a tuple of any combinations of items that adds up to the target
 
@@ -142,9 +137,8 @@ def simple_subset_sum(items, target: int) -> tuple:
             if sum(subset) == target:
                 yield subset
                 
-
 if __name__ == "__main__":
     t1 = time.perf_counter()
     main()
     t2 = time.perf_counter()
-    print(f"Execution time: {t2 - t1:0.4f} seconds")
+    logger.info("Execution time: %.3f seconds", t2 - t1)
